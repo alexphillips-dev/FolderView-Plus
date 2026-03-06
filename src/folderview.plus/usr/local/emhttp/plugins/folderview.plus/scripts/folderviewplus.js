61f54b7c1abc70793494ff4a90216d3d5e0cdb7d
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-03-05 19:41:26 -0500

    Increase settings header title visibility

--- src/folderview.plus/usr/local/emhttp/plugins/folderview.plus/scripts/folderviewplus.js
+++ src/folderview.plus/usr/local/emhttp/plugins/folderview.plus/scripts/folderviewplus.js
@@ -873,7 +873,8 @@ const initSettingsControls = () => {
     controls.html(`
         <div class="fv-settings-inline">
             <div class="fv-settings-left" aria-label="Plugin settings title">
-                <span class="fv-settings-title">FolderView Plus Settings</span>
+                <h2 class="fv-settings-title">FolderView Plus</h2>
+                <span class="fv-settings-subtitle">Plugin settings</span>
             </div>
             <div class="fv-settings-right">
                 <input type="text" id="fv-settings-search" placeholder="Search settings" aria-label="Search settings">

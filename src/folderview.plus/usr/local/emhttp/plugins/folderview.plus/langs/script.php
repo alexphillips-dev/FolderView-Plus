5438b8002ff031b1b645cfd32a1752a08f7adc80
Alexander Phillips <alexphillips-dev@users.noreply.github.com>
2026-04-05 13:02:55 -0400

    Fix plugin asset cache busting

--- src/folderview.plus/usr/local/emhttp/plugins/folderview.plus/langs/script.php
+++ src/folderview.plus/usr/local/emhttp/plugins/folderview.plus/langs/script.php
@@ -1,29 +1,30 @@
 <?php
+    require_once('/usr/local/emhttp/plugins/folderview.plus/server/lib.php');
     if($_SESSION['locale'] == "") {
         $loc = 'en'; 
     } else {
         $loc = substr($_SESSION['locale'], 0, 2);
     }
+    $localeAssets = [
+        'en' => fvplus_versioned_plugin_asset_path('/plugins/folderview.plus/langs/en.json')
+    ];
+    if ($loc != 'en') {
+        $localeAssets[$loc] = fvplus_versioned_plugin_asset_path("/plugins/folderview.plus/langs/$loc.json");
+    }
 ?>
-<script src="/plugins/folderview.plus/scripts/include/CLDRPluralRuleParser.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.messagestore.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.fallbacks.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.language.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.parser.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.emitter.js"></script>
-<script src="/plugins/folderview.plus/scripts/include/jquery.i18n.emitter.bidi.js"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/CLDRPluralRuleParser.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.messagestore.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.fallbacks.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.language.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.parser.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.emitter.js')?>"></script>
+<script src="<?php fvplus_asset('/plugins/folderview.plus/scripts/include/jquery.i18n.emitter.bidi.js')?>"></script>
 <script>
     if(typeof folderi18n === 'undefined' ) {
         folderi18n = () => {};
     }
     $.i18n({
-        'locale': '<? echo $loc?>'
-    }).load({
-        <?php
-            if($loc != 'en') {
-                echo "'$loc': '/plugins/folderview.plus/langs/$loc.json',";
-            }
-        ?>'en': '/plugins/folderview.plus/langs/en.json'
-    }).then(folderi18n, ()=>{});
+        'locale': '<?php echo $loc?>'
+    }).load(<?php echo json_encode($localeAssets, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE); ?>).then(folderi18n, ()=>{});
 </script>
